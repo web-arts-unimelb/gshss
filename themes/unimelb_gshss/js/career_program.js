b264e5a94b1219e0bd70b6aff1c9986b9987d0f9
arts-webmaster <web.arts.unimelb@gmail.com>
2012-09-06 22:47:33 +1000

    adjust quicktab style, but need more work

---
+++ themes/unimelb_gshss/js/career_program.js
@@ -0,0 +1,77 @@
+jQuery(document).ready(function($) {
+	/*
+		* Handle careers by program
+		* Sample html
+		* We use uom-row-class as a start point
+		
+		<h3><a href="xxxx">Eum Valde</a></h3>
+  		<div class="views-row views-row-1 views-row-odd views-row-first views-row-last uom-row-class">
+  			<div class="views-field views-field-field-career-outcomes">        
+				<div class="field-content">
+					<div class="item-list">
+						<ul>
+							<li class="first">Corporate communications</li>
+							<li>Advocacy</li>
+							<li>Advocacy</li>
+							<li class="last">Public service</li>
+						</ul>
+					</div>
+				</div>  
+			</div>  
+		</div>
+	*/
+
+	// Initial
+	var target_class_name = "." + "careers-by-program-row";
+	$(target_class_name).prev().find("a").removeAttr("href");
+	$(target_class_name + " .item-list").hide();
+
+	// Listen
+	$(target_class_name).prev().find("a").click(function(){
+		$(this).parent().next().find(".item-list").toggle();
+	});
+
+
+	/*
+		<h3><a href="http://gshss.local/#">Defence</a></h3>
+  		<div class="views-row views-row-1 views-row-odd views-row-first programs-by-career-row">
+			<a href="http://gshss.local/executive-master-arts-0">Executive Master of Arts</a>    
+        </div>
+
+		<div class="views-row views-row-2 views-row-even programs-by-career-row">
+      		<a href="http://gshss.local/9nfcxyhaxgpksvnmufk5nezdoywndiyy5vhmbbmnbpgj54de3gtmuqqytvl5aaq4jq6clkuqxbkpwxr4qu9ducgppdgozguwytlt">9NFcxyhAXGPksVNMufk5NEzDoywNdiYY5vHMbBmnbpGJ54dE3GTMUqQYtVL5aAq4jQ6CLkUqxbkpWXR4qu9DuCGpPDgozGuwYTLTNNhX4SYZ3SFY2aYLtkUAjVDXZtdfRrAAHQh23wFTehaAnof9LWoCFY4h3kJDDgLpUbQCwsXXeqJiSxLKmTdkmGtXNVgwxYPffwFCKxwZYyu6pNxGaMhC5yRP9ELL8XKrB45pDpM4npGKWqLe6GCRw45Reku</a>    
+        </div>
+
+		<div class="views-row views-row-3 views-row-odd views-row-last programs-by-career-row">
+			<a href="http://gshss.local/anzevlsknbbb5a6dxvtjxcdw6thn5hwsoumgeazyb6nbu2spwkxmq66ceesy9rhfgrdfwjxydjtw7pqb6fk3qgzido4papfnmftu">aNZEVLskNBbb5A6dXvtjXcdW6thN5HWSoUMgEAzYB6NbU2spWkXMq66CeEsy9RhfgRdFWjxYdJtW7pQb6FK3qGziDo4pApfNMFtUyXqLEuTYjqJN3qY73VGkdLCVJxaKPW6EYTkEf3GrmAdGE9vWmr2BJzbhc5vdbaPoUyw84cLaMejpvHneJpAinT4fZz8LZSEUKGMixkKBrZJiK4HpGcstPHDEt2JQfZheyoBgscBLbh95oNqPFxXTkqhGFis</a>    
+        </div>
+	*/
+	
+	// Initial
+	var target_class_name = "." + "programs-by-career-row";
+	var first_row = ".views-row-1" + target_class_name; 
+	$(first_row).prev().find("a").removeAttr("href");
+	$(target_class_name).hide();
+
+	// Listen
+	$(first_row).prev().find("a").click(function(){
+		var curr_element = $(this).parent().next();
+		var tag_name = curr_element.get(0).tagName;
+		if(tag_name == "DIV")
+		{
+			// First div
+			curr_element.toggle();
+
+			// 2nd div
+			while(curr_element.next().get(0).tagName == "DIV")
+			{
+				curr_element.next().toggle();
+				curr_element = curr_element.next();
+			}
+		}
+		else
+		{
+
+		}
+	});
+});
