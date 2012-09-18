a3bd281e85b550185d2a47207b60a3bc5a3efcf1
arts-webmaster <web.arts.unimelb@gmail.com>
2012-09-18 16:13:37 +1000

    use browscap_get_browser() from browscap

--- themes/unimelb_gshss/template.php
+++ themes/unimelb_gshss/template.php
@@ -109,9 +109,9 @@ function __get_social_icon_dimensions()
 	$info["width"] = "30px";
 	$info["height"] = "30px";
 
-	$browser_info = __get_browser_info();
-	$user_agent = $browser_info["name"]; 
-	if($user_agent == "Google Chrome")
+	$browser_info = browscap_get_browser();
+	$user_agent = $browser_info["browser"]; 
+	if($user_agent == "Chromium")
 	{
 		$info["width"] = "28px";
 		$info["height"] = "28px";	
@@ -129,9 +129,9 @@ function __get_social_icon_adjust()
 	$info = array();
 	$info["padding-top"] = "3px";
 
-	$browser_info = __get_browser_info();
-	$user_agent = $browser_info["name"]; 
-	if($user_agent == "Google Chrome")
+	$browser_info = browscap_get_browser();
+	$user_agent = $browser_info["browser"]; 
+	if($user_agent == "Chromium")
 	{
 		$info["padding-top"] = "2px";	
 	}	
@@ -142,92 +142,3 @@ function __get_social_icon_adjust()
 
 	return $info;
 }
-
-// See: http://php.net/manual/en/function.get-browser.php
-function __get_browser_info()
-{
-    $u_agent = $_SERVER['HTTP_USER_AGENT'];
-    $bname = 'Unknown';
-    $platform = 'Unknown';
-    $version= "";
-
-    //First get the platform?
-    if (preg_match('/linux/i', $u_agent)) {
-        $platform = 'linux';
-    }
-    elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
-        $platform = 'mac';
-    }
-    elseif (preg_match('/windows|win32/i', $u_agent)) {
-        $platform = 'windows';
-    }
-   
-    // Next get the name of the useragent yes seperately and for good reason
-    if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
-    {
-        $bname = 'Internet Explorer';
-        $ub = "MSIE";
-    }
-    elseif(preg_match('/Firefox/i',$u_agent))
-    {
-        $bname = 'Mozilla Firefox';
-        $ub = "Firefox";
-    }
-    elseif(preg_match('/Chrome/i',$u_agent))
-    {
-        $bname = 'Google Chrome';
-        $ub = "Chrome";
-    }
-    elseif(preg_match('/Safari/i',$u_agent))
-    {
-        $bname = 'Apple Safari';
-        $ub = "Safari";
-    }
-    elseif(preg_match('/Opera/i',$u_agent))
-    {
-        $bname = 'Opera';
-        $ub = "Opera";
-    }
-    elseif(preg_match('/Netscape/i',$u_agent))
-    {
-        $bname = 'Netscape';
-        $ub = "Netscape";
-    }
-   
-    // finally get the correct version number
-    $known = array('Version', $ub, 'other');
-    $pattern = '#(?<browser>' . join('|', $known) .
-    ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-    if (!preg_match_all($pattern, $u_agent, $matches)) {
-        // we have no matching number just continue
-    }
-   
-    // see how many we have
-    $i = count($matches['browser']);
-    if ($i != 1) {
-        //we will have two since we are not using 'other' argument yet
-        //see if version is before or after the name
-        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
-            $version= $matches['version'][0];
-        }
-        else {
-            $version= $matches['version'][1];
-        }
-    }
-    else {
-        $version= $matches['version'][0];
-    }
-   
-    // check if we have a number
-    if ($version==null || $version=="") {$version="?";}
-   
-    return array(
-        'userAgent' => $u_agent,
-        'name'      => $bname,
-        'version'   => $version,
-        'platform'  => $platform,
-        'pattern'    => $pattern
-    );
-}
-
-
